cd0ef2306cc3517f4a4396682891095d1b416416
Mahmoud Kamal Fawzy <65209396+mahmoud947@users.noreply.github.com>
2023-03-01 21:04:15 +0200

    integrate with camerax

--- app/src/main/java/com/example/demofacedetectionwithmediapipe/MainActivity.kt
+++ app/src/main/java/com/example/demofacedetectionwithmediapipe/MainActivity.kt
@@ -1,15 +1,136 @@
 package com.example.demofacedetectionwithmediapipe
 
+import android.graphics.SurfaceTexture
 import android.os.Bundle
+import android.util.Size
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import com.google.mediapipe.components.CameraHelper.CameraFacing
+import com.google.mediapipe.components.CameraXPreviewHelper
+import com.google.mediapipe.components.ExternalTextureConverter
+import com.google.mediapipe.components.FrameProcessor
+import com.google.mediapipe.components.PermissionHelper
+import com.google.mediapipe.framework.AndroidAssetUtil
+import com.google.mediapipe.glutil.EglManager
 
 class MainActivity : AppCompatActivity() {
     private val BINARY_GRAPH_NAME = "face_detection_mobile_gpu.binarypb"
     private val INPUT_VIDEO_STREAM_NAME = "input_video"
     private val OUTPUT_VIDEO_STREAM_NAME = "output_video"
 
+
+    private var previewFrameTexture: SurfaceTexture? = null
+    private var previewDisplayView: SurfaceView? = null
+    private var eglManager: EglManager? = null
+    private var processor: FrameProcessor? = null
+    private var converter: ExternalTextureConverter? = null
+    private var cameraHelper: CameraXPreviewHelper? = null
+    private val CAMERA_FACING = CameraFacing.FRONT
+
+    private val FLIP_FRAMES_VERTICALLY = true
+
+    companion object {
+        init {
+            // Load all native libraries needed by the app.
+            System.loadLibrary("mediapipe_jni")
+            System.loadLibrary("opencv_java3")
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        previewDisplayView = SurfaceView(this)
+        setupPreviewDisplayView()
+
+        // Initialize asset manager so that MediaPipe native libraries can access the app assets, e.g.,
+        // binary graphs.
+        AndroidAssetUtil.initializeNativeAssetManager(this)
+
+        eglManager = EglManager(null)
+        processor = FrameProcessor(
+            this,
+            eglManager!!.getNativeContext(),
+           BINARY_GRAPH_NAME,
+            INPUT_VIDEO_STREAM_NAME,
+           OUTPUT_VIDEO_STREAM_NAME
+        )
+        processor!!.getVideoSurfaceOutput().setFlipY(FLIP_FRAMES_VERTICALLY)
+
+        PermissionHelper.checkAndRequestCameraPermissions(this)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        converter = ExternalTextureConverter(eglManager!!.context)
+        converter!!.setFlipY(FLIP_FRAMES_VERTICALLY)
+        converter!!.setConsumer(processor)
+        if (PermissionHelper.cameraPermissionsGranted(this)) {
+            startCamera()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        converter!!.close()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        PermissionHelper.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+
+    private fun setupPreviewDisplayView() {
+        previewDisplayView!!.visibility = View.GONE
+        val viewGroup = findViewById<ViewGroup>(R.id.preview_display_layout)
+        viewGroup.addView(previewDisplayView)
+        previewDisplayView!!
+            .holder
+            .addCallback(
+                object : SurfaceHolder.Callback {
+                    override fun surfaceCreated(holder: SurfaceHolder) {
+                        processor!!.videoSurfaceOutput.setSurface(holder.surface)
+                    }
+
+                    override fun surfaceChanged(
+                        holder: SurfaceHolder,
+                        format: Int,
+                        width: Int,
+                        height: Int
+                    ) {
+
+                        val viewSize = Size(width, height)
+                        val displaySize = cameraHelper!!.computeDisplaySizeFromViewSize(viewSize)
+
+                        converter!!.setSurfaceTextureAndAttachToGLContext(
+                            previewFrameTexture, displaySize.width, displaySize.height
+                        )
+                    }
+
+                    override fun surfaceDestroyed(holder: SurfaceHolder) {
+                        processor!!.videoSurfaceOutput.setSurface(null)
+                    }
+                })
+    }
+
+
+    private fun startCamera() {
+        cameraHelper = CameraXPreviewHelper()
+        cameraHelper!!.setOnCameraStartedListener { surfaceTexture ->
+            previewFrameTexture = surfaceTexture
+            previewDisplayView!!.visibility = View.VISIBLE
+        }
+        cameraHelper!!.startCamera(this, CAMERA_FACING, null)
     }
 }
---
+++ app/src/main/java/com/example/demofacedetectionwithmediapipe/Test.java
@@ -0,0 +1,8 @@
+package com.example.demofacedetectionwithmediapipe;
+
+public class Test {
+    static {
+        System.loadLibrary("mediapipe_jni");
+        System.loadLibrary("opencv_java3");
+    }
+}
